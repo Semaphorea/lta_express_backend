067c1c6d6901732c96e68d7b61b24b4a11780d3c
Semaphorea <102250023+Semaphorea@users.noreply.github.com>
2024-04-27 19:44:44 +0200

    Update environnement.ts

--- Configuration/Environnement/environnement.ts
+++ Configuration/Environnement/environnement.ts
@@ -4,21 +4,20 @@ export const environnment = {
   frontendURL: 'localhost:4200/',
   apiKey: 'devKey',
   env: 'dev',
-  paypalAPI: { 
-    CLIENT_ID_PAYPAL: 'AS18aeFMAxFih_2bYiHEpfmDzSKum0m6G7k8QC2R3l9fQ8bQAnAsTdZfM2DKhIGmRgUR75rXZumtDh79',
-    // APP_SECRET_PAYPAL:'ECIB5oknLBhfgdjY58yMGYj6heF_5JwT3JjcLXt5PuaMHPtd0LVEPCw-HmCm0P4qQIba59vemXOKN92'
-    APP_SECRET_PAYPAL: 'ECIB5oknLBhfgdjYJ8yMGYj6heF_5JwT3JjcLXt5PuaMHPtd0LVEPCw-HmCm0P4qQIba59vemXOKN92i',
+  paypalAPI: {
+    CLIENT_ID_PAYPAL: 'personnal_id_paypal',
+    APP_SECRET_PAYPAL: 'personnal_secret_paypal',
     paypalURL: "https://api-m.sandbox.paypal.com"
   },
-  mailsender: { 
+  mailsender: {
     addressrecipient:'semaphorea@protonmail.com',  
-    mailtrap:{
-    SMTPCredential:'bd1f252ebafcea23ac5c9cfd4663d721',
-    SMTPUsername: 'c5574eeecb636b',
-    SMTPPassword: 'c6e5fe42b61c0b',
-    SMTPServer: 'sandbox.smtp.mailtrap.io',
-    SMTPPort : 2525,  
-  },
+    mailtrap : {
+      SMTPCredential:'bd1f252ebafcea23ac5c9cfd4663d721', 
+      SMTPuser: 'personnal_identifiant',
+      SMTPpassword: 'password',
+      SMTPServer: 'sandbox.smtp.mailtrap.io',
+      SMTPPort :2525, //25,467,587 possibles 
+    },
   gmail:{
      SMTPCredential : "",
      SMTPServer : "smtp.gmail.com",
