4cba39aef99c0c8046a558c0a47a269c86641c86
Semaphorea <102250023+Semaphorea@users.noreply.github.com>
2024-04-27 19:43:00 +0200

    Update environnement.dev.ts

--- Configuration/Environnement/environnement.dev.ts
+++ Configuration/Environnement/environnement.dev.ts
@@ -5,16 +5,16 @@ export const environnment = {
   apiKey: 'devKey',
   env: 'dev',
   paypalAPI: {
-    CLIENT_ID_PAYPAL: 'AS18aeFMAxFih_2bYiHEpfmDzSKum0m6G7k8QC2R3l9fQ8bQAnAsTdZfM2DKhIGmRgUR75rXZumtDh79',
-    APP_SECRET_PAYPAL: 'ECIB5oknLBhfgdjY58yMGYj6heF_5JwT3JjcLXt5PuaMHPtd0LVEPCw-HmCm0P4qQIba59vemXOKN92',
+    CLIENT_ID_PAYPAL: 'personnal_id_paypal',
+    APP_SECRET_PAYPAL: 'personnal_secret_paypal',
     paypalURL: "https://api-m.sandbox.paypal.com"
   },
   mailsender: {
     addressrecipient:'semaphorea@protonmail.com',  
     mailtrap : {
       SMTPCredential:'bd1f252ebafcea23ac5c9cfd4663d721', 
-      SMTPuser: 'c5574eeecb636b',
-      SMTPpassword: 'c6e5fe42b61c0b',
+      SMTPuser: 'personnal_identifiant',
+      SMTPpassword: 'password',
       SMTPServer: 'sandbox.smtp.mailtrap.io',
       SMTPPort :2525, //25,467,587 possibles 
     },
